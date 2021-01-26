bd063e33c48913c5f632237dd6801013919d5494
Gabriel Vazquez <gabvazquez@ucdavis.edu>
2021-01-25 21:44:03 -0800

    renaming dir

---
+++ ASTAR_ShortestPath/MtStHelensDiv_917307919.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import java.util.List;
+
+public class MtStHelensDiv_917307919 implements AIModule{
+    @Override
+    public List<Point> createPath(TerrainMap map) {
+        // use first cost function
+        return null;
+    }
+}
