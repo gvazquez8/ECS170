a41a511486fe52e8b2a3c720618d54f0a34b4a04
Gabriel Vazquez <gabvazquez@ucdavis.edu>
2021-03-13 13:09:39 -0800

    Removing files and updating .gitignore

--- ASTAR_ShortestPath/MtStHelensExp_917307919.java
+++
@@ -1,9 +0,0 @@
-import java.awt.*;
-import java.util.List;
-
-public class MtStHelensExp_917307919 implements AIModule{
-    @Override
-    public List<Point> createPath(TerrainMap map) {
-        return null;
-    }
-}
