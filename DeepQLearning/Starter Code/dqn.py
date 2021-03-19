89192a47160f5f3ad9ec4878df04bdad6f49dbad
gvazquez8 <gabvazquez@ucdavis.edu>
2021-03-18 23:10:20 -0700

    Adding assignment 3 submission

--- DeepQLearning/Starter Code/dqn.py
+++ DeepQLearning/Starter Code/dqn.py
@@ -5,8 +5,11 @@ import torch
 import torch.nn as nn
 import torch.autograd as autograd
 import math, random
+
 USE_CUDA = torch.cuda.is_available()
-Variable = lambda *args, **kwargs: autograd.Variable(*args, **kwargs).cuda() if USE_CUDA else autograd.Variable(*args, **kwargs)
+Variable = lambda *args, **kwargs: autograd.Variable(*args, **kwargs).cuda() if USE_CUDA else autograd.Variable(*args,
+                                                                                                                **kwargs)
+
 
 class QLearner(nn.Module):
     def __init__(self, env, num_frames, batch_size, gamma, replay_buffer):
@@ -42,17 +45,16 @@ class QLearner(nn.Module):
         return x
 
     def feature_size(self):
-            return self.features(autograd.Variable(torch.zeros(1, *self.input_shape))).view(1, -1).size(1)
+        return self.features(autograd.Variable(torch.zeros(1, *self.input_shape))).view(1, -1).size(1)
 
     def act(self, state, epsilon):
         if random.random() > epsilon:
             state = Variable(torch.FloatTensor(np.float32(state)).unsqueeze(0), requires_grad=True)
             # TODO: Given state, you should write code to get the Q value and chosen action
 
-
-            action = random.randrange(self.env.action_space.n)
-
-
+            # self(state) - returns Q-values of the state
+            # torch.argmax(self(state)) - returns index of largest q value. represents action to make
+            action = torch.argmax(self(state))
 
         else:
             action = random.randrange(self.env.action_space.n)
@@ -65,14 +67,22 @@ class QLearner(nn.Module):
 def compute_td_loss(model, target_model, batch_size, gamma, replay_buffer):
     state, action, reward, next_state, done = replay_buffer.sample(batch_size)
 
-    state = Variable(torch.FloatTensor(np.float32(state)))
+    state = Variable(torch.FloatTensor(np.float32(state)).squeeze(1))
     next_state = Variable(torch.FloatTensor(np.float32(next_state)).squeeze(1), requires_grad=True)
     action = Variable(torch.LongTensor(action))
     reward = Variable(torch.FloatTensor(reward))
     done = Variable(torch.FloatTensor(done))
-    # implement the loss function here
 
+    q = model(state).gather(1, action.unsqueeze(-1)).squeeze(-1)
 
+    y = reward
+
+    try:
+        y[done == 0] += gamma * target_model(next_state)[(done == 0)].detach().max(1)[0]
+    except RuntimeError as e:
+        pass
+
+    loss = nn.MSELoss(reduction="sum")(y, q)
 
     return loss
 
@@ -90,6 +100,23 @@ class ReplayBuffer(object):
     def sample(self, batch_size):
         # TODO: Randomly sampling data with specific batch size from the buffer
 
+        # Sample batch_size times
+        sample_batch = random.sample(self.buffer, batch_size)
+
+        state = []
+        action = []
+        reward = []
+        next_state = []
+        done = []
+
+        # add each sample into lists
+        for sample in sample_batch:
+            s, a, r, ns, d = sample
+            state.append(s)
+            action.append(a)
+            reward.append(r)
+            next_state.append(ns)
+            done.append(d)
 
         return state, action, reward, next_state, done
 
